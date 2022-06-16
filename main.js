47e0608538045a8d3dc11d0a7d7499dc292031de
Emiliano Becerra <emibecerra92@gmail.com>
2022-06-16 11:03:13 -0300

    js 16062022

---
+++ main.js
@@ -0,0 +1,13 @@
+let saludo = document.querySelector(".saludo");
+
+let nombre = prompt ("¡hola! ¿cúal es tu nombre?");
+
+
+if (typeof nombre === "string"){
+    saludo.textContent = "Bienvenidx " + nombre + " :)";
+} else {
+    saludo.textContent = "Bienvenidx"; 
+}
+
+
+
